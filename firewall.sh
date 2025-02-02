f90975c9afa69899dfcb51464948a5b3efd7b68f
shoham1246 <shoham.marom@campus.technion.ac.il>
2025-02-02 15:54:01 +0200

    bash updated

--- firewall.sh
+++ firewall.sh
@@ -3,16 +3,14 @@
 
 #user input
 complexRules=$(cat $1) #read rules file from first arg
-packetsInput=$(cat -) #read packets file from stdin
-validPackets= #final list
+packetsInput=$(cat - | sed 's/  //g') #read packets file from stdin
+validPackets="" #final list
 
 # ========= Part one - parsing 1 complex rule
 applyComplexRule() {
 	local complexRuleFourFields=$1
-	local PacketsInput=$(cat -)
-	#for a valid rule line template: 1. no comments 2. no commas
-	local validRuleFourFields=$(cat "$complexRuleFourFields" | grep -vE "^ *#"  | cut -d "#" -f 1 |sed 's/,/ /g')
-	for field in $validRuleFourFields; do #bash's IFS interepts words as fields - white space(s) separting
+	local PacketsInput=$2
+	for field in $complexRuleFourFields; do #bash's IFS interepts words as fields - white space(s) separting
 		PacketsInput=$(echo "$PacketsInput" | ./firewall.exe "$field")
 	done
 	echo "$PacketsInput" | sort | uniq
@@ -20,11 +18,13 @@ applyComplexRule() {
 
 
 # ========= Part two - the wrap - parsing all complex rules
-#for a valid rules file - no empty lines
-validRules=$(cat "$complexRules" | grep -v '^$')
-for Rule in $validRules; do #bash's IFS interepts lines as rules
-	validPackets+=$(echo "$packetsInput" | applyComplexRule "$Rule")
+#for a valid rule line template: 1. no comments 2. no commas
+complexRules=$(echo "$complexRules" | grep -vE "^ *#"  | cut -d "#" -f 1 |sed 's/,/ /g')
+for Rule in $complexRules; do #bash's IFS interepts lines as rules
+	validPackets+=$(applyComplexRule "$Rule" "$packetsInput")
 	validPackets+="\n"
 done
+#no empty lines needed
+validPackets=$(echo -e "$validPackets" | grep -v '^$')
 
 echo -e "$validPackets" | sort | uniq
